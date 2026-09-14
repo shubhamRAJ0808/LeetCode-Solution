b5a6967cfd5a201ec5e2a18184a836ba74cccae1
Shubham Raj <shubham123678raj@gmail.com>
2026-09-14 19:27:26 +0530

    Time: 30 ms (75.23%) | Memory: 55.3 MB (28.82%) - LeetSync

---
+++ 937-online-stock-span/online-stock-span.java
@@ -0,0 +1,21 @@
+class StockSpanner {
+
+    Stack<int[]> st;
+
+    public StockSpanner() {
+        st = new Stack<>();
+    }
+
+    public int next(int price) {
+
+        int span = 1;
+
+        while (!st.isEmpty() && st.peek()[0] <= price) {
+            span = span + st.pop()[1];
+        }
+
+        st.push(new int[]{price, span});
+
+        return span;
+    }
+}
